b2d9109002e0c2fa5cc36a6183462ae480efb83c
Lucas <153649430+LCVNI@users.noreply.github.com>
2025-11-18 23:12:31 -0300

    update

--- jmm.c
+++ jmm.c
@@ -3,7 +3,7 @@
 #include<time.h>
 #include<string.h>
 #include<unistd.h> //Para a funcao sleep
-
+#include<stdbool.h>
 typedef struct {//struct de jogador
     char nome[80];
     int pontos;
@@ -18,36 +18,54 @@ int acha_pos(Player players[], int tamanho) {
     }
     return -1; // cheio
 }
+
 //Funcao que adiciona player
 void addPlayer(Player players[], int tamanho) {
     int pos = acha_pos(players, tamanho);
-
     if (pos == -1) {
         printf("Lista cheia!\n");
         return;
     }
-
+    FILE *lista= fopen("lista.txt","a");
     printf("Nome do novo jogador: ");
     fgets(players[pos].nome, sizeof(players[pos].nome), stdin);
-
     players[pos].pontos = 0;
+    players[pos].nome[strcspn(players[pos].nome, "\n")] = '\0';
+    fprintf(lista, "%s %d\n", players[pos].nome, players[pos].pontos);
+    fclose(lista);
 }
 
 //Funcao de login
 void login(){
+    FILE *lista= fopen("lista.txt","r");
+
     Player players[11]={0};
+    char nomeArquivo[80];
+    int pontos;
+    bool chamaadd = true;
     printf("Bem vindo!\n");
     printf("Nome de Jogador: ");
     fgets(players[0].nome, sizeof(players[0].nome), stdin);
-    for(int i=1; i < 11; i++){
-        if(!strcmp(players[0].nome, players[i].nome)){
-            printf("Seja bem vindo de volta %s!", players[i].nome);
-            printf("Seu recorde foi: %d", players[i].pontos);
+
+   while (fgets(nomeArquivo, sizeof(nomeArquivo), lista) != NULL) {
+
+    fscanf(lista, "%79s %d", nomeArquivo, &pontos);
+    players[0].nome[strcspn(players[0].nome, "\n")] = '\0'; // Tirar o \n
+    nomeArquivo[strcspn(nomeArquivo, "\n")] = '\0'; // Tirar o \n
+
+        if (!strcmp(players[0].nome, nomeArquivo)) {
+            printf("Bem-Vindo %s!\n",nomeArquivo);
+            printf("Seu maior recorde foi: %d pontos!\n",pontos);
+            chamaadd = false;
             break;
         }
+
     }
+    fclose(lista);
+    if(chamaadd){
     printf("Nenhum jogador com esse nick encontrado!\n");
     addPlayer(players, 11);
+    }
 }
 
 char *simbols={"ZWXP@$C#89F2HGM"}; // Simbolos que vao aparecer nas cartas
@@ -103,19 +121,20 @@ void geracartas(int tam, char *simbols){ //Funcao que gera as cartas
         }
     printf("\n");
     }
+
     for(int i = 3; i >= 0; i--){
         sleep(1);
         printf("\r%d...", i); // Contador de tempo
 
     }
     system("cls");
+
     for(int i=0; i < tam; i++){
         for(int j=0; j < tam; j++){
             printf("[?]");
         }
     printf("\n");
     }
-    system("Pause");
 }
 
 
@@ -161,9 +180,11 @@ void dificuldade(){ //Funcao para o usuario escolher o nivel de jogo
 
 }
 
+void escolhecarta(){
+    
+}
 
 int main(){
-login();
    struct player;
    int menu;
     do{
@@ -177,23 +198,25 @@ login();
     switch(menu){
 
         case 1:
+        getchar(); // Evitar problemas com o buffer \n
+        login();
         dificuldade();
-        
+        escolhecarta();
         break;
-
         case 2:
-        printf("Chamar funcao do jogo contra");
+        printf("Chamar modo 2 jogdores");
         break;
-
         case 3:
         printf("\nChamar funcao que mostra o ranking (Arquivo) ");
         break;
-
+        case 4:
+        printf("Saindo...");
+        break;
         default:
         printf("Opcao inválida!");
         break;
     }
-    }while(menu == 4);
+    }while(menu != 4);
     printf("\nSaiu!");
     return 0;
 }
